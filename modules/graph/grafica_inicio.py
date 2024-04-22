18bfb2ac2f5de12ff9b1f17b505db3034d0b8637
RubenMaestre <formacion@rubenmaestre.com>
2024-04-22 15:21:29 +0200

    Update grafica_inicio.py

--- modules/graph/grafica_inicio.py
+++ modules/graph/grafica_inicio.py
@@ -1,10 +1,9 @@
 #modules/graph/grafica_inicio.py
-import numpy as np
 import plotly.graph_objects as go
 import streamlit as st
 from modules.cargar_df import cargar_df
 
-
+# Cargar datos
 df, df_continentes = cargar_df()
 
 def grafica_rango_years(df_continentes):
@@ -15,15 +14,13 @@ def grafica_rango_years(df_continentes):
 
     # Define manualmente los años para los ticks del eje x
     specified_years = [-10000, -5000, -2000, -1000, -500, -200, 0, 200, 500, 800, 
-                       1000, 1200, 1400, 1600, 1800, 1850, 1900, 1950, 2000, 2020, 2050, 2100]
-    
-    # Asegúrate de que solo se usen los años que existen en el DataFrame para los tickvals
+                       1000, 1500, 1800, 1900, 2000, 2020, 2050, 2100]
+
+    # Filtrar años que realmente existen en el DataFrame para los tickvals
     tickvals = [year for year in specified_years if year in df_continentes['Year'].values]
-    
-    # Asigna las etiquetas de ticktext
     ticktext = [f"{abs(year)} a.C." if year < 0 else f"{year} d.C." for year in tickvals]
 
-    # Ordenar el DataFrame por 'Year' para asegurarse de que la gráfica se renderice correctamente
+    # Ordenar el DataFrame por 'Year'
     df_continentes.sort_values(by='Year', inplace=True)
 
     # Crear la figura
@@ -32,17 +29,21 @@ def grafica_rango_years(df_continentes):
     # Añadir trazos para cada entidad
     for entity in df_continentes['Entity'].unique():
         df_entity = df_continentes[df_continentes['Entity'] == entity]
-        fig.add_trace(go.Scatter(
-            x=df_entity['Year_Label'],  # Se mantiene el uso de 'Year_Label' para el eje x
-            y=df_entity['Population density'],
-            mode='lines',
-            name=entity
-        ))
+        if not df_entity.empty:
+            fig.add_trace(go.Scatter(
+                x=df_entity['Year_Label'],
+                y=df_entity['Population density'],
+                mode='lines',
+                name=entity
+            ))
 
-    # Actualizar los ticks del eje X
-    fig.update_xaxes(tickvals=tickvals, ticktext=ticktext)
+    # Configuración de los ticks del eje X
+    fig.update_xaxes(
+        tickvals=[df_continentes[df_continentes['Year'] == year]['Year_Label'].iloc[0] for year in tickvals if year in df_continentes['Year'].values],
+        ticktext=ticktext
+    )
 
-    # Configurar el layout de la figura
+    # Configuración del layout de la figura
     fig.update_layout(
         title='Evolución de la densidad de población por zona geográfica',
         xaxis_title='Año',
@@ -51,9 +52,7 @@ def grafica_rango_years(df_continentes):
         xaxis=dict(
             tickmode='array',
             type='linear',
-            showgrid=True,
-            # Asegúrate de que el dominio del eje X cubra los años especificados
-            range=[min(specified_years), max(specified_years)]
+            showgrid=True
         )
     )
 
