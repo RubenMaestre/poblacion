a0273cd42c8aa7825bb2e1b66a8d098d4b549362
RubenMaestre <formacion@rubenmaestre.com>
2024-04-22 11:41:34 +0200

    Update inicio.py

--- paginas/inicio.py
+++ paginas/inicio.py
@@ -18,15 +18,15 @@ def display():
     st.markdown("<br><br>", unsafe_allow_html=True)  # Espacio extra
 
     # Título de la página
-    st.markdown("<h1 style='text-align: center;'>Estudio sobre la población mundial</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>Estudio sobre la densidad de población mundial</h1>", unsafe_allow_html=True)
     st.markdown("<br><br>", unsafe_allow_html=True)
 
     # Introducción descriptiva
     st.markdown("""
         <div style='text-align: justify;'>
-            Bienvenidos a la página dedicada al estudio de la evolución de la población humana en los continentes y países 
+            Bienvenidos a este proyecto sobre la evolución de la densidad de población humana en los continentes y países 
             a lo largo de los últimos 12,000 años. Este análisis permite observar las tendencias históricas en la 
-            densidad de población y entender cómo han cambiado las dinámicas poblacionales a través de las eras. 
+            densidad de población y entender cómo han cambiado las dinámicas poblacionales a través tiempo. 
             A continuación, presentamos una visualización interactiva que muestra estos cambios a lo largo del tiempo.
         </div>
         """, unsafe_allow_html=True)
