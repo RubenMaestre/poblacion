43cae2793c566c670e991a8bb2b7c8e8008dfa88
RubenMaestre <formacion@rubenmaestre.com>
2024-04-22 11:36:17 +0200

    Update inicio.py

--- paginas/inicio.py
+++ paginas/inicio.py
@@ -35,12 +35,15 @@ def display():
     df_continentes['Year_Label'] = df_continentes['Year'].apply(lambda x: f"{abs(x)} a.C." if x < 0 else f"{x} d.C.")
 
     if df_continentes is not None:
-        fig = px.line(df_continentes, x='Year_Label', y='Population density', color={'Entity': 'Zona geográfica'}, 
+        fig = px.line(df_continentes, x='Year_Label', y='Population density', color='Entity', 
                       title='Evolución de la densidad de población por zona geográfica',
-                      labels={'Population density': 'Densidad de población (personas por km²)', 'Year_Label': 'Año'})
+                      labels={
+                          'Population density': 'Densidad de población (personas por km²)',
+                          'Year_Label': 'Año',
+                          'Entity': 'Zona geográfica'  # Esto cambiará la etiqueta de la leyenda para 'Entity'
+                      })
 
         # Usar esta función para integrar la gráfica en Streamlit
         st.plotly_chart(fig, use_container_width=True)
 
-    # Espacio al final de la página
     st.markdown("<br><br>", unsafe_allow_html=True)
