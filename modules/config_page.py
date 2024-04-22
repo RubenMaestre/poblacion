88d90ff9fd721abac4a7493263a0d4d01ac4200c
RubenMaestre <formacion@rubenmaestre.com>
2024-04-22 15:43:12 +0200

    Update config_page.py

--- modules/config_page.py
+++ modules/config_page.py
@@ -4,6 +4,6 @@ import streamlit as st
 def set_global_page_config():
     st.set_page_config(
         page_title="Proyecto sobre densidad de población mundial",
-        page_icon="🌍👥",
+        page_icon="🌍",
         layout="wide"
     )
