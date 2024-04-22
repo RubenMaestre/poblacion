54a794549b3d5d22be708188e135f984b9fb5ae2
RubenMaestre <formacion@rubenmaestre.com>
2024-04-22 16:07:57 +0200

    Update top_20_density.py

--- modules/graph/top_20_density.py
+++ modules/graph/top_20_density.py
@@ -3,39 +3,22 @@ import pandas as pd
 import streamlit as st
 
 def plot_top_20_density(df):
-    # Filtrar el DataFrame para el año deseado y calcular el top 20 en densidad
-    df_year = df[df['Year'] == 2024]
-    top_20 = df_year.nlargest(20, 'Population density')
+    # Filtrar el DataFrame para el año 2024
+    df_2024 = df[df['Year'] == 2024]
+    
+    # Ordenar por densidad de población de mayor a menor y tomar los primeros 20
+    top_20_density = df_2024.nlargest(20, 'Population density')
 
-    # Crear la figura
-    fig = go.Figure()
-
-    # Añadir burbujas para los top 20 países en densidad
-    for i, row in top_20.iterrows():
-        # Tamaño del círculo en función de la densidad de población
-        size = row['Population density']
-        fig.add_trace(go.Scatter(
-            x=[row['Entity']],  # Nombre del país
-            y=[size],           # Usar la densidad como la coordenada y para la dispersión vertical
-            marker=dict(
-                size=size,      # El tamaño del marcador es proporcional a la densidad
-                sizemode='area',  # El tamaño del marcador representa el área (densidad de población)
-                sizeref=2.*max(df_year['Population density'])/(40.**2),  # Ajuste para el tamaño de referencia
-            ),
-            mode='markers',
-            name=row['Entity']
-        ))
-
-    # Actualizar el layout para mejor visualización
-    fig.update_layout(
-        title='Top 20 países por densidad de población en 2024',
-        xaxis=dict(showticklabels=True),
-        yaxis=dict(showticklabels=False),
-        showlegend=False
+    # Crear un gráfico de barras con Plotly Express
+    fig = px.bar(
+        top_20_density,
+        x='Entity',  # Nombres de los países
+        y='Population density',  # Densidad de población
+        title='Top 20 Países por Densidad de Población en 2024',
+        labels={'Entity': 'País', 'Population density': 'Densidad de Población (personas por km²)'},
+        color='Population density',  # Color en función de la densidad
+        height=900
     )
 
-    # Ajustar la configuración de los ejes para mejor visualización
-    fig.update_xaxes(tickangle=45)
-
-    # Mostrar la figura en Streamlit
-    st.plotly_chart(fig, use_container_width=True)
+    # Ordenar las barras de mayor a menor
+    fig.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
