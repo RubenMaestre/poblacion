ba7fdb4415c8c602ed0dcbbd6a1cf000a9abbc88
RubenMaestre <formacion@rubenmaestre.com>
2024-04-23 15:27:20 +0200

    Update grafica_densidad_mundo.py

--- modules/graph/grafica_densidad_mundo.py
+++ modules/graph/grafica_densidad_mundo.py
@@ -1,20 +1,29 @@
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
 def plot_population_density_map_with_plotly(df):
     df_2024 = df[df['Year'] == 2024].dropna(subset=['Population density'])
     df_2024 = df_2024[~df_2024['Code'].str.startswith('OWID')]
     
+    # Agregar una nueva columna con el logaritmo de la densidad de población
+    df_2024['Log Population Density'] = np.log1p(df_2024['Population density'])  # log1p para manejar el logaritmo de 0
+    
+    # Normalizar la densidad logarítmica de 0 a 100
+    max_log_density = df_2024['Log Population Density'].max()
+    min_log_density = df_2024['Log Population Density'].min()
+    df_2024['Normalized Log Density'] = 100 * (df_2024['Log Population Density'] - min_log_density) / (max_log_density - min_log_density)
+
     # Crear el mapa coroplético con Plotly Express
     fig = px.choropleth(
         df_2024,
         locations="Code",  # Columna con los códigos de país ISO
-        color="Population density",  # Usamos la columna de densidad de población real
+        color="Normalized Log Density",  # Columna que define el color de la choropleth
         hover_name="Entity",  # Asumiendo que 'Entity' es la columna con los nombres de los países
-        hover_data={"Population density": ":.2f"},  # Mostrar la densidad real con 2 decimales
+        hover_data={"Population density": ":.2f",  # Mostrar la densidad real con 2 decimales
+                    "Normalized Log Density": False},  # Ocultar la densidad logarítmica normalizada en el tooltip
         color_continuous_scale=['white', 'red'],  # Escala de color de blanco a rojo
         projection="natural earth",  # Proyección del mapa
-        range_color=[0, 10],  # Rango de colores para la leyenda de 0 a 10
     )
 
     fig.update_layout(
@@ -28,7 +37,8 @@ def plot_population_density_map_with_plotly(df):
         height=600   # Altura del mapa
     )
 
-    # Ajustar la leyenda para que muestre "Índice de densidad de población"
+    # Ajustar la leyenda para que muestre "Índice de Densidad de Población"
     fig.update_coloraxes(colorbar_title="Índice de densidad de población")
 
     return fig
+
