a8bfd610e4ca50afe33596b589ecccc2c8b67e2e
RubenMaestre <formacion@rubenmaestre.com>
2024-04-23 12:14:35 +0200

    Update grafica_densidad_mundo_folium.py

--- modules/graph/grafica_densidad_mundo_folium.py
+++ modules/graph/grafica_densidad_mundo_folium.py
@@ -11,12 +11,12 @@ def plot_population_density_map_with_folium(df, width=1200, height=500):
     min_density = df_2023['Population density'].min()
     max_density = df_2023['Population density'].max()
 
-    # Define el número de pasos en la escala de colores
-    steps = 100
-
-    # Crea una escala de colores lineal
-    colormap = cm.linear.YlOrRd_09.scale(min_density, max_density).to_step(steps)
+    # Define una escala de colores
+    colormap = cm.linear.YlOrRd_09.scale(min_density, max_density)
     colormap.caption = 'Population Density'
+
+    # Crea una lista de umbrales manualmente
+    threshold_scale = np.linspace(min_density, max_density, 6).tolist()  # Ajusta a 6 u otro número si deseas más intervalos
 
     # Crea un mapa base
     m = folium.Map(location=[20, 0], tiles='cartodbpositron', zoom_start=2, width=width, height=height)
@@ -30,12 +30,12 @@ def plot_population_density_map_with_folium(df, width=1200, height=500):
         fill_color='YlOrRd',
         fill_opacity=0.7,
         line_opacity=0.2,
-        threshold_scale=colormap.colors,  # Usa la escala de colores creada
+        threshold_scale=threshold_scale,  # Usa la lista de umbrales creada
         reset=True
     ).add_to(m)
 
     # Añade la escala de colores al mapa
-    m.add_child(colormap)
+    colormap.add_to(m)
 
     # Añade el control de capas al mapa
     folium.LayerControl().add_to(m)
