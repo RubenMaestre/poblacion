4e62dcc786c9b00946e0ddf1dcfbc393ca4ea84b
RubenMaestre <formacion@rubenmaestre.com>
2024-04-23 15:22:40 +0200

    Update grafica_densidad_mundo.py

--- modules/graph/grafica_densidad_mundo.py
+++ modules/graph/grafica_densidad_mundo.py
@@ -3,31 +3,37 @@ import pandas as pd
 import numpy as np
 
 def plot_population_density_map_with_plotly(df):
-    df_2023 = df[df['Year'] == 2023].dropna(subset=['Population density'])
-    df_2023 = df_2023[~df_2023['Code'].str.startswith('OWID')]
+    df_2024 = df[df['Year'] == 2024].dropna(subset=['Population density'])
+    df_2024 = df_2024[~df_2024['Code'].str.startswith('OWID')]
     
-    # Agregar una nueva columna con el logaritmo de la densidad de población
-    df_2023['Log Population Density'] = np.log1p(df_2023['Population density'])  # log1p para manejar el logaritmo de 0
+    # Suponiendo que tu DataFrame tiene una columna llamada 'Country Name' con los nombres de los países.
+    # Si no es así, deberás agregarla o ajustar esta línea según corresponda.
 
     # Crear el mapa coroplético con Plotly Express
     fig = px.choropleth(
-        df_2023,
+        df_2024,
         locations="Code",  # Columna con los códigos de país ISO
-        color="Log Population Density",  # Columna que define el color de la choropleth
-        hover_name="Code",  # Columna para mostrar en el tooltip
+        color="Population density",  # Usamos la columna de densidad de población real
+        hover_name="Entity",  # Asumiendo que 'Entity' es la columna con los nombres de los países
+        hover_data={"Log Population Density": False,  # Ocultar la densidad logarítmica
+                    "Population density": ":.2f"},  # Mostrar la densidad real con 2 decimales
         color_continuous_scale=['white', 'red'],  # Escala de color de blanco a rojo
         projection="natural earth",  # Proyección del mapa
+        range_color=[0, 10],  # Rango de colores para la leyenda de 0 a 10
     )
 
     fig.update_layout(
-        title_text='Densidad de Población por País en 2023',
+        title_text='Densidad de población por país en 2024',
         title_x=0.5,  # Centrar el título
         geo=dict(
             showframe=False,  # Ocultar el marco del mapa
             showcoastlines=False,  # Ocultar las líneas costeras
         ),
         width=1200,  # Ancho del mapa
-        height=600   # Altura del mapa, ajusta esto como necesites
+        height=600   # Altura del mapa
     )
+
+    # Ajustar la leyenda para que muestre "Índice de Densidad de Población"
+    fig.update_coloraxes(colorbar_title="Índice de densidad de población")
 
     return fig
