618439d017d1f91a7c5fdd3894c3f35ff96341e8
RubenMaestre <formacion@rubenmaestre.com>
2024-04-23 11:04:59 +0200

    actualiza

---
+++ modules/graph/grafica_densidad_mundo.py
@@ -0,0 +1,26 @@
+import folium
+from streamlit_folium import folium_static
+
+def plot_population_density_map(df):
+    # Filtrar el DataFrame para el año 2024
+    df_2024 = df[df['Year'] == 2024]
+
+    # Inicializar un mapa del mundo
+    m = folium.Map(location=[20, 0], tiles="cartodbpositron", zoom_start=2)
+
+    # Usar un Choropleth para visualizar la densidad de población
+    folium.Choropleth(
+        geo_data='https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json',
+        name='choropleth',
+        data=df_2024,
+        columns=['Code', 'Population density'],
+        key_on='feature.id',
+        fill_color='YlOrRd',
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name='Densidad de población (personas por km²)',
+        nan_fill_color='white'  # Color para países sin datos
+    ).add_to(m)
+
+    # Mostrar el mapa en Streamlit
+    folium_static(m)
--- paginas/densidad.py
+++ paginas/densidad.py
@@ -3,6 +3,7 @@ import streamlit as st
 import plotly.graph_objects as go
 from modules.cargar_df import cargar_df
 from modules.graph.top_20_density import plot_top_20_density
+from modules.graph.grafica_densidad_mundo import plot_population_density_map
 
 def display():
     df, df_continentes, df_ingresos = cargar_df()
@@ -14,6 +15,9 @@ def display():
     # Título de la página
     st.markdown("<h1 style='text-align: center;'>Densidad</h1>", unsafe_allow_html=True)
     st.markdown("<br><br>", unsafe_allow_html=True)
+    st.write("En esta página vas a encontrar datos prácticamente en brutos sobre la densidad de población. Quiero mostarlos como una primera toma de contacto en lo que va a ser el proyecto.")
 
     plot_top_20_density(df)
+    st.markdown("<br><br>", unsafe_allow_html=True)
+    plot_population_density_map(df)
     
