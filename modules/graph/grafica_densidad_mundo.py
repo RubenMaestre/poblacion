8ad994dbd33f3a1e1e40d38c8f29e0a1988f936b
RubenMaestre <formacion@rubenmaestre.com>
2024-04-23 11:49:28 +0200

    Update grafica_densidad_mundo.py

--- modules/graph/grafica_densidad_mundo.py
+++ modules/graph/grafica_densidad_mundo.py
@@ -1,22 +1,24 @@
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
 def plot_population_density_map_with_plotly(df):
     df_2023 = df[df['Year'] == 2023].dropna(subset=['Population density'])
     df_2023 = df_2023[~df_2023['Code'].str.startswith('OWID')]
+    
+    # Agregar una nueva columna con el logaritmo de la densidad de población
+    df_2023['Log Population Density'] = np.log1p(df_2023['Population density'])  # log1p para manejar el logaritmo de 0
 
     # Crear el mapa coroplético con Plotly Express
     fig = px.choropleth(
         df_2023,
         locations="Code",  # Columna con los códigos de país ISO
-        color="Population density",  # Columna que define el color de la choropleth
+        color="Log Population Density",  # Columna que define el color de la choropleth
         hover_name="Code",  # Columna para mostrar en el tooltip
         color_continuous_scale=px.colors.sequential.Plasma,  # Escala de color
         projection="natural earth",  # Proyección del mapa
+        # range_color se omite, ya que la escala logarítmica se aplicará al usar los valores de log
     )
-
-    fig.update_coloraxes(colorscale="Plasma", colorbar_title="Log Density",
-                         type='log')  # Aplicar escala logarítmica
 
     fig.update_layout(
         title_text='Densidad de Población por País en 2023',
