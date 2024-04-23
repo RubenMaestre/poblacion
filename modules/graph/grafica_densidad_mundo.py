97dbfe79070f00daf2c619051b9c598af45edadb
RubenMaestre <formacion@rubenmaestre.com>
2024-04-23 11:32:20 +0200

    Update grafica_densidad_mundo.py

--- modules/graph/grafica_densidad_mundo.py
+++ modules/graph/grafica_densidad_mundo.py
@@ -1,17 +1,18 @@
 import folium
 from streamlit_folium import folium_static
-import streamlit as st
 import pandas as pd
-import numpy as np  # Asegúrate de importar numpy
+import numpy as np
 
 def plot_population_density_map(df):
     df_2023 = df[df['Year'] == 2023].dropna(subset=['Population density'])
     df_2023 = df_2023[~df_2023['Code'].str.startswith('OWID')]
 
-    # Calculando percentiles para definir los bins
-    percentiles = np.percentile(df_2023['Population density'], [10, 30, 50, 70, 90])
-    print("Percentiles para bins:", percentiles)
-    print(df_2023['Population density'].describe())  # Diagnóstico estadístico
+    # Encuentra el mínimo y máximo en los datos
+    min_density = df_2023['Population density'].min()
+    max_density = df_2023['Population density'].max()
+
+    # Crear bins que cubran todo el rango de datos
+    bins = np.linspace(min_density, max_density, num=6)  # Crea 5 intervalos desde el mínimo al máximo
 
     m = folium.Map(location=[20, 0], tiles="cartodbpositron", zoom_start=2, width='100%', height='80%')
 
@@ -25,7 +26,7 @@ def plot_population_density_map(df):
         fill_opacity=0.7,
         line_opacity=0.2,
         legend_name='Densidad de población (personas por km²)',
-        threshold_scale=list(percentiles),  # Utilizando percentiles como escalas
+        threshold_scale=list(bins),  # Usa los bins ajustados
         nan_fill_color='white'  # Color para países sin datos
     ).add_to(m)
 
