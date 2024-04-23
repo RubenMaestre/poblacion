e8ad6faf34a1152102466a42747c949b2a9b1c1e
RubenMaestre <formacion@rubenmaestre.com>
2024-04-23 12:09:48 +0200

    Update grafica_densidad_mundo_folium.py

--- modules/graph/grafica_densidad_mundo_folium.py
+++ modules/graph/grafica_densidad_mundo_folium.py
@@ -6,46 +6,38 @@ import branca.colormap as cm
 def plot_population_density_map_with_folium(df, width=1200, height=500):
     df_2023 = df[df['Year'] == 2023].dropna(subset=['Population density'])
     df_2023 = df_2023[~df_2023['Code'].str.startswith('OWID')]
-    
-    # Calcula el logaritmo de la densidad de población para reducir el impacto de valores extremadamente altos
     df_2023['Log Population Density'] = np.log1p(df_2023['Population density'])
 
-    # Define los límites de la escala de colores en función de los datos logarítmicos
     min_density_log = df_2023['Log Population Density'].min()
     max_density_log = df_2023['Log Population Density'].max()
 
-    # Define el número de pasos en la escala de colores
     steps = 100
+    threshold_scale = np.linspace(min_density_log, max_density_log, steps).tolist()
 
     # Crea una escala de colores lineal
-    colormap = cm.linear.YlOrRd_09.scale(min_density_log, max_density_log)
-    
-    # Crea una lista de umbrales utilizando el número de pasos definido
-    threshold_scale = np.linspace(min_density_log, max_density_log, steps).tolist()
+    colormap = cm.linear.YlOrRd_09.scale(min_density_log, max_density_log).to_step(steps)
+    colormap.caption = 'Log of Population Density'  # Título de la escala de colores
 
     # Crea un mapa base
     m = folium.Map(location=[20, 0], tiles='cartodbpositron', zoom_start=2, width=width, height=height)
-
     
-    # Añade la escala de colores al mapa
-    colormap.add_to(m)
-
-    # Añade un mapa coroplético al mapa base usando los datos logarítmicos
-    folium.Choropleth(
+    # Crea y añade el mapa coroplético al mapa base
+    choropleth = folium.Choropleth(
         geo_data='https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json',
-        name='choropleth',
         data=df_2023,
         columns=['Code', 'Log Population Density'],
         key_on='feature.id',
         fill_color='YlOrRd',
         fill_opacity=0.7,
         line_opacity=0.2,
-        legend_name='Log of Population Density',
         threshold_scale=threshold_scale,  # Usa la escala de umbrales creada
         reset=True
     ).add_to(m)
+    
+    # Agrega la escala de colores personalizada al mapa
+    m.add_child(colormap)
 
-    # Añade la leyenda al mapa
+    # Añade el control de capas al mapa
     folium.LayerControl().add_to(m)
 
     return m
