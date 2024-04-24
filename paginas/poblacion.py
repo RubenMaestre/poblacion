d3e99cbf5559f196bfa742879cb665dd093b45f3
RubenMaestre <formacion@rubenmaestre.com>
2024-04-24 10:32:02 +0200

    Update poblacion.py

--- paginas/poblacion.py
+++ paginas/poblacion.py
@@ -7,6 +7,6 @@ from modules.cargar_df_pob import cargar_df_pob
 
 def display():
     #df_paises_pob, df_continentes_pob, df_sociedad_pob, df_mundo_pob = cargar_df_pob()
-    st.markdown("# Página de Población")
-    st.write("Si ves este mensaje, la página está cargando correctamente.")
+    st.markdown("# Población mundial")
+    st.write("En construcción")
 
